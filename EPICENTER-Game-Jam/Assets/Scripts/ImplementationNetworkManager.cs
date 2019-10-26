cc94830734ad075004beaafbbc348b3d11c60cc7
Rok Kos <rok.kos@outfit7.com>
2019-10-26 15:39:37 +0200

    Implementation Network Manager draft and player spawning

---
+++ EPICENTER-Game-Jam/Assets/Scripts/ImplementationNetworkManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class ImplementationNetworkManager : NetworkManager {
+    public override void OnServerConnect(NetworkConnection conn) {
+        base.OnServerConnect(conn);
+        Debug.Log("<color=green>ImplementationNetworkManager::</color> A client connected to the server: " + conn);
+    }
+}

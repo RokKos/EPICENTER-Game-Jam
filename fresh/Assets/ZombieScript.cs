e7e2821c1fba6280221fe484fd70fa8950a91ab9
Katja Rutnik <katja.rutnik1@student.um.si>
2019-10-27 12:28:05 +0100

    update

---
+++ fresh/Assets/ZombieScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieScript : MonoBehaviour
+{    
+    public GameObject Player;
+    public float movementSpeed = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+
+
+}

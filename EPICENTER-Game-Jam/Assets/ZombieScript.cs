3935f95765c2a5d76d6063da577626810723ac9c
Timotej Grabušnik <timotej.grabusnik@student.um.si>
2019-10-27 15:02:39 +0100

    Added zombie spawning

---
+++ EPICENTER-Game-Jam/Assets/ZombieScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieScript : MonoBehaviour
+{
+    public GameObject Player;
+    public float movementSpeed = 4;
+
+    void Update() {
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+
+    private void OnParticleCollision(GameObject other) {
+        Destroy(this.gameObject);
+    }
+}
---
+++ fresh/Assets/spawnZombies.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnZombies : MonoBehaviour
+{
+
+    private float nextSpawn = 0.0f;
+    public float spawnCheckDelay = 3.0f;
+
+    public GameObject zombie;
+
+    void Start() {
+    }
+
+    void Update() {
+        if (Time.time > nextSpawn) {
+            nextSpawn += spawnCheckDelay;
+
+            if (numberOfZombies() < 5) {
+                spawnZombie();
+            }
+        }
+    }
+
+    int numberOfZombies() {
+        return GameObject.FindGameObjectsWithTag("Zombie").Length;
+    }
+
+    void spawnZombie() {
+        int x = Random.Range(-20, 20);
+        int z = Random.Range(-20, 20);
+
+        if (x < 16 || z > -13) {
+            Instantiate(zombie, new Vector3(x, 0.5f, z), Quaternion.identity);
+        }
+    }
+}

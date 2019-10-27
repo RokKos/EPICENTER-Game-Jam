f793c6e441da400b9be9acc72699b3f5bfe7c650
Katja Rutnik <katja.rutnik1@student.um.si>
2019-10-27 16:02:56 +0100

    Bullets

--- fresh/Assets/Scenes/ZombieScript.cs
+++ fresh/Assets/Scenes/ZombieScript.cs
@@ -10,15 +10,27 @@ public class ZombieScript : MonoBehaviour
     public Transform player;
     public NavMeshAgent agent;
 
+    public int health = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.destination = Player.transform.position;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.collider.tag == "Bullet"){
+        health--;
+        if (health <= 0) {
+            Destroy(this.gameObject);
+        }
+        }
     }
 }
